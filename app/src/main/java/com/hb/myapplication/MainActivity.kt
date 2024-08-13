d3184b06b12e7b6f59be023856a6cc67d10c6b05
Nguyen Ba Hung <hung@bio-rithm.com>
2024-08-13 11:02:27 +0700

    Random background color based on night mode

--- app/src/main/java/com/hb/myapplication/MainActivity.kt
+++ app/src/main/java/com/hb/myapplication/MainActivity.kt
@@ -2,30 +2,49 @@ package com.hb.myapplication
 
 import android.Manifest
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import leoh.screenshot.protector.ScreenshotProtector
+import leoh.screenshot.protector.extension.isNightMode
+import java.util.Random
 
 open class MainActivity : AppCompatActivity() {
     lateinit var textView: TextView
+    private lateinit var container: ViewGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         ScreenshotProtector.protect(this)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        container = findViewById(R.id.main)
+        ViewCompat.setOnApplyWindowInsetsListener(container) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        container.setBackgroundColor(randomBackgroundColor())
         textView = findViewById(R.id.tvMessage)
         textView.setOnClickListener {
             startActivity(Intent(this, SecondActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun randomBackgroundColor(): Int {
+        val rnd = Random()
+        val range = 120
+        if (isNightMode) {
+            return Color.rgb(rnd.nextInt(range), rnd.nextInt(range), rnd.nextInt(range))
+        } else {
+            val base = 255 - range
+            return Color.rgb(base + rnd.nextInt(range), base + rnd.nextInt(range), base + rnd.nextInt(range))
         }
     }
 
--- app/src/main/java/com/hb/myapplication/SecondActivity.kt
+++ app/src/main/java/com/hb/myapplication/SecondActivity.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 class SecondActivity : MainActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        textView.setOnClickListener {
-            showConfirmationDialog()
-        }
         showConfirmationDialog()
         requestOnePermission()
     }
